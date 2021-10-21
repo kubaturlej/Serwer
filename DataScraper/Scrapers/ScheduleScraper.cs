9aa02fa44ee1755db97fa7c4662c23ee3675d3bc
kubinio <kuba.turlej@wp.pl>
2021-10-21 14:28:54 +0200

    Scraper Refactor

--- DataScraper/Scrapers/ScheduleScraper.cs
+++ DataScraper/Scrapers/ScheduleScraper.cs
@@ -21,93 +21,73 @@ namespace DataScraper.Scrapers
             var web = new HtmlWeb();
             var document = web.Load(BaseUrl + _league);
 
-            var roundsList = new List<Round>();
             var matches = new List<Match>();
             var round = new Round();
+
+            var roundsList = new List<Round>();
 
             var table = document.QuerySelectorAll("table.standard_tabelle")[0];
 
             var trs = table.QuerySelectorAll("tr");
 
 
+
             if (_league == "all_matches/bundesliga-2021-2022/" || _league == "all_matches/pol-ekstraklasa-2021-2022/")
             {
-                string data = string.Empty;
-                string time = string.Empty;
-                for (int i = 0; i < trs.Count; i++)
-                {
-                    if (i % 10 == 0)
-                    {
-                        matches.Clear();
-                        round = new Round();
-                        round.RoundNumber = trs[i].InnerText.Trim();
-                    }
-                    else
-                    {
-                        var tds = trs[i].QuerySelectorAll("td");
-
-                        if (!string.IsNullOrEmpty(tds[0].InnerText))
-                        {
-                            data = tds[0].InnerText;
-                        }
-
-                        if (!string.IsNullOrEmpty(tds[1].InnerText))
-                        {
-                            time = tds[1].InnerText;
-                        }
-
-                        matches.Add(new Match { Date = data, Time = time, FirstTeam = tds[2].InnerText, SecondTeam = tds[4].InnerText, Score = tds[5].InnerText.Trim() });
-                    }
-                    if ((i + 1) % 10 == 0 && i > 0)
-                    {
-                        foreach (var match in matches)
-                        {
-                            round.Matches.Add(match);
-                        }
-                        roundsList.Add(round);
-                    }
-                }
+                ScrapData(matches, round, roundsList, trs, 10);
             }
             else
             {
-                string data = string.Empty;
-                string time = string.Empty;
-                for (int i = 0; i < trs.Count; i++)
-                {
-                    if (i % 11 == 0)
-                    {
-                        matches.Clear();
-                        round = new Round();
-                        round.RoundNumber = trs[i].InnerText.Trim();
-                    }
-                    else
-                    {
-                        var tds = trs[i].QuerySelectorAll("td");
-  
-                        if (!string.IsNullOrEmpty(tds[0].InnerText))
-                        {
-                            data = tds[0].InnerText;
-                        }
-
-                        if (!string.IsNullOrEmpty(tds[1].InnerText))
-                        {
-                            time = tds[1].InnerText;
-                        }
-
-                        matches.Add(new Match { Date = data, Time = time, FirstTeam = tds[2].InnerText, SecondTeam = tds[4].InnerText, Score = tds[5].InnerText.Trim() });
-                    }
-                    if ((i + 1) % 11 == 0 && i > 0)
-                    {
-                        foreach (var match in matches)
-                        {
-                            round.Matches.Add(match);
-                        }
-                        roundsList.Add(round);
-                    }
-                }
+                ScrapData(matches, round, roundsList, trs, 11);
             }
 
             return roundsList;
+        }
+
+        private static void ScrapData(List<Match> matches, Round round, List<Round> roundsList, IList<HtmlNode> trs, int modulo)
+        {
+            var data = string.Empty;
+            var time = string.Empty;
+            for (int i = 0; i < trs.Count; i++)
+            {
+                if (i % modulo == 0)
+                {
+                    matches.Clear();
+                    round = new Round();
+                    round.RoundNumber = trs[i].InnerText.Trim();
+                }
+                else
+                {
+                    var tds = trs[i].QuerySelectorAll("td");
+
+                    if (!string.IsNullOrEmpty(tds[0].InnerText))
+                    {
+                        data = tds[0].InnerText;
+                    }
+
+                    if (!string.IsNullOrEmpty(tds[1].InnerText))
+                    {
+                        time = tds[1].InnerText;
+                    }
+
+                    matches.Add(new Match
+                    {
+                        Date = data,
+                        Time = time,
+                        FirstTeam = tds[2].InnerText,
+                        SecondTeam = tds[4].InnerText,
+                        Score = tds[5].InnerText.Trim()
+                    });
+                }
+                if ((i + 1) % modulo == 0 && i > 0)
+                {
+                    foreach (var match in matches)
+                    {
+                        round.Matches.Add(match);
+                    }
+                    roundsList.Add(round);
+                }
+            }
         }
     }
 }
